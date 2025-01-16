6e9e5bcae071087c1f66fd351e08d3a822930a3a
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2025-01-15 21:56:39 -0800

    Add NUnit test project

---
+++ EmploymentManagementTests/MainTests.cs
@@ -0,0 +1,16 @@
+namespace EmploymentManagementTests
+{
+    public class MainTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
